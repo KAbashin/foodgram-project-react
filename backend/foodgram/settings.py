f65119e0620fa832b021533e924325b74a2c906d
Константин Абашин <kabashin@mail.ru>
2022-10-18 17:25:13 +0300

    Diplom Part:1 CRv1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = (
 
 DEBUG = os.getenv('DEBUG', default='True') == 'True'
 
-ALLOWED_HOSTS = ['*', 'ALLOWED_HOSTS']
+ALLOWED_HOSTS = os.getenv('*', 'ALLOWED_HOSTS')
 
 
 AUTH_USER_MODEL = 'users.User'
@@ -66,12 +66,13 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': os.getenv('DB_ENGINE',
+                            default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
